5c6e0b260b207d199e3ff633d56d8d7b8b64aabe
deadmarstton <deadmarston@163.com>
2019-04-15 13:39:55 +0800

    now we have a little material system which could deal with lambertian, metal(reflect) and dielectric(reflect and refract), Coolgit status!

--- pathrender/material.h
+++ pathrender/material.h
@@ -23,13 +23,54 @@ public:
 
 class metal :public material {
 public:
-	metal(const vec3& a) : albedo(a) {};
+	metal(const vec3& a, float f) : albedo(a) { if (f < 1) fuzz = f; else fuzz = 1; };
 	virtual bool scatter(const ray& ir, const hit_record& rec, vec3& attenuation, ray& scattered) const{
 		vec3 reflected = reflect(unit_vector(ir.direction()), rec.normal);
-		scattered = ray(rec.p, reflected);
+		scattered = ray(rec.p, reflected + fuzz*random_in_unit_sphere());
 		attenuation = albedo;
 		return (dot(scattered.direction(), rec.normal) > 0);
 	}
 
 	vec3 albedo;
+	float fuzz;
+};
+
+class dielectirc : public material {
+public:
+	dielectirc(float ri) :ref_idx(ri) {};
+	virtual bool scatter(const ray& r, const hit_record& rec, vec3& attenuation, ray& scattered) const {
+		vec3 outward_normal;
+		vec3 reflected = reflect(r.direction(), rec.normal);
+		float ni_over_nt;
+		attenuation = vec3(1.0, 1.0, 1.0);
+		vec3 refracted;
+		float reflect_prob;
+		float cosine;
+
+		if (dot(r.direction(), rec.normal) > 0) {
+			outward_normal = -rec.normal;
+			ni_over_nt = ref_idx;
+			cosine = ref_idx * dot(r.direction(), rec.normal) / r.direction().length();
+		}
+		else {
+			outward_normal = rec.normal;
+			ni_over_nt = 1.0 / ref_idx;
+			cosine = -dot(r.direction(), rec.normal) / r.direction().length();
+		}
+
+		if (refract(r.direction(), outward_normal, ni_over_nt, refracted)) {
+			reflect_prob = schlick(cosine, ref_idx);
+		}
+		else {
+			reflect_prob = 1.0;
+		}
+		if (random_func() < reflect_prob) {
+			scattered = ray(rec.p, reflected);
+		}
+		else {
+			scattered = ray(rec.p, refracted);
+		}
+		return true;
+	}
+	float ref_idx;
 };
--- pathrender/ray.h
+++ pathrender/ray.h
@@ -14,9 +14,27 @@ public:
 	vec3 b;
 };
 
+float schlick(float cosine, float ref_idx){
+	float r0 = (1 - ref_idx) / (1 + ref_idx);
+	r0 = r0 * r0;
+	return r0 + (1 - r0)*pow((1 - cosine), 5);
+}
 vec3 reflect(const vec3& in, const vec3& n) {
 	vec3 out = in - 2 * dot(in, n)*n;
 	return out;
+}
+
+bool refract(const vec3& v, const vec3& n, float ni_over_nt, vec3& refracted) {
+	vec3 uv = unit_vector(v);
+	float dt = dot(uv, n);
+	float discriminant = 1.0 - ni_over_nt * ni_over_nt * (1 - dt*dt);
+	if (discriminant > 0) {
+		refracted = ni_over_nt * (uv - dt *n) - n*sqrt(discriminant);
+		return true;
+	}
+	else {
+		return false;
+	}
 }
 
 float random_func(int precision = 10000, bool leftIncluded = false) {
