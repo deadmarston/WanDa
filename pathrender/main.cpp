7e7b027546f8d70aeefc6758c8010b858f7dcdeb
deadmarstton <deadmarston@163.com>
2019-05-18 10:52:46 +0900

    a full cornell box

--- pathrender/main.cpp
+++ pathrender/main.cpp
@@ -12,6 +12,7 @@
 #include "bvh.h"
 #include "texture.h"
 #include "rect.h"
+#include "box.h"
 
 #include "testJson.h"
 
@@ -157,8 +158,8 @@ hitable *cornell_box() {
 	list[i++] = new flip_normals(new xz_rect(0, 555, 0, 555, 555, white));
 	list[i++] = new xz_rect(0, 555, 0, 555, 0, white);
 	list[i++] = new flip_normals(new xy_rect(0, 555, 0, 555, 555, white));
-	return new hitable_list(list, i);
-
+	list[i++] = new translate(new rotate_y(new box(vec3(0, 0, 0), vec3(165, 165, 165), white), -18), vec3(130, 0, 65));
+	list[i++] = new translate(new rotate_y(new box(vec3(0, 0, 0), vec3(165, 330, 165), white), 15), vec3(265, 0, 295));
 	return new hitable_list(list, i);
 }
 
